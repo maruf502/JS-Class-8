9f831eaff4f79166793adc67f197571d52433a40
Maruf Khan <marufatrai12@gmail.com>
2023-11-15 11:44:20 +0600

    JS-Class-8

---
+++ Javascript/script.js
@@ -0,0 +1,99 @@
+
+
+
+
+    //  Arithmetic Operator ->      +, -, *, /, %           (++), (--), (**),
+    
+    //  Assignment Operator ->      =, +=, -=, *=, /=, %=, **=
+    
+    
+
+/*
+    var x = 5;
+    var y = 5;
+
+    var z = x+y;
+
+    document.write(z); // output 10
+
+*/
+
+/*
+    
+    var x,y,z;
+
+    x = 50;
+    y = 30;
+
+    z = x-y;
+
+    document.write(z); // OutPut 20
+*/
+
+/*
+
+    var x = 5;
+    var y = 5;
+
+    var z = x*y;
+
+    document.write(z); // OutPut 25 
+
+*/
+
+/*
+    var x = 4;
+    var y = 2;
+
+    var z = x/y;
+
+    document.write(z); // OutPut 2
+
+*/
+
+/*
+
+    var x = 4;
+
+    x += 6  // x = 4 + 6 = 10 
+
+    document.write(x); //   OutPut 10
+
+    */
+
+/*  
+    var y = 10;
+
+    y-= 5; // y = 10 - 5 = 5
+
+    document.write(y); // OutPut 5
+
+*/
+
+/*
+    var z = 10;
+
+    z*=2    // z = 10 * 2 = 20
+
+    document.write(z);  // OutPut 20
+
+*/
+
+/*
+    var m = 4
+
+    m /= 2  // m = 4 / 2 = 2 
+
+    document.write(m);  // OutPut 2
+
+*/
+
+/*
+
+    var n = 4 ;
+
+    n %= 2  // n = 4 % 2 = 0
+
+    document.write(n);  //Output 0
+    
+*/
